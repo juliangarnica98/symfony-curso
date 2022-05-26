48677da24e489d3fd0517ff457c5527267a5f839
Julian Garnica <juliang1012@gmail.com>
2022-05-26 03:19:01 -0500

    Added some Tiwggy goodness

--- src/Controller/CursoController.php
+++ src/Controller/CursoController.php
@@ -2,16 +2,29 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class CursoController
+class CursoController extends AbstractController
 {
     #[Route('/')]
-    public function homepage(): Response
+    public function homepage()
     {
-        return $this->render('');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        return $this->render('curso/base.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
         //return new Response('Title : Curse');
     }
 
