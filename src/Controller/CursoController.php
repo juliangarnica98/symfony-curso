84ebcf94fe27d6e28541712b265beef73df67389
Julian Garnica <juliang1012@gmail.com>
2022-05-26 02:10:40 -0500

    urls y controladores

---
+++ src/Controller/CursoController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
+class CursoController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return new Response('Title : Curse');
+    }
+
+    #[Route('/rol/{slug}')]
+    public function rol(String $slug = null): Response
+    {
+        if($slug){
+            $role = 'Role: '.u(str_ireplace('-',' ',$slug))->title(true);
+        }else{
+            $role = 'All roles';
+        }
+        return new Response($role);
+    }
+}
